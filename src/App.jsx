70e53c9c0f463d6c610b2701523388e54720de90
agustina mosqueda <brisaagustina_@outlook.com>
2024-02-04 21:46:40 -0300

    primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import {useState} from "react"
+// import Navbar from "./components/layout/Navbar"
+// import { productos, Navbar } from "./components/layout/Navbar"
+
+import { Counter } from "./components/common/Counter"
+import { Navbar } from "./components/layout/Navbar"
+import ItemListContainer from "./components/pages/ItemListContainer"
+
+const App = ()=>{
+
+  const [ darkMode , setDarkMode  ] = useState(false)
+
+  
+
+  const cambiarModo = ()=>{
+
+    setDarkMode(  !darkMode  )
+
+  }
+
+  console.log( darkMode )
+
+  return (
+    <div>
+      <button onClick={cambiarModo}>De modo claro a modo oscuro</button>
+    {/* <Navbar />
+     <ItemListContainer /> */}
+    <Counter />
+    </div>
+  )
+}
+
+export default App
